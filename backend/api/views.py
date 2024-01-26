b645c8193874a9f01fce2e62b91fc83fd36de0ec
hridesh <hridesh699@gmail.com>
2024-01-27 01:02:27 +0530

    fixed import bug

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.crawler import CrawlerManager
-from api.custom_views import APIView
 from api.serializers import SearchSerializer, UrlSerializer
 from utils import get_db
 
